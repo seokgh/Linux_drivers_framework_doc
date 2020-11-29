89c4d9024816c427fbaeed4113d5e91a7a2840dc
seokgh <shigh1005@gmail.com>
2020-11-28 21:53:47 -1000

    Add files via upload

---
+++ ftrace/rpi4_ftrace_test/irq_time_trace.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+sleep 1
+
+echo "tracing_off"
+
+echo 0 > /sys/kernel/debug/tracing/events/enable
+sleep 1
+echo "events disabled"
+
+echo secondary_start_kernel > /sys/kernel/debug/tracing/set_ftrace_filter
+sleep 1
+echo "set_ftrace_filter init"
+
+echo function_graph > /sys/kernel/debug/tracing/current_tracer
+sleep 1
+echo "function_graph tracer enabled"
+
+echo bcm2835_mmc_irq > /sys/kernel/debug/tracing/set_ftrace_filter
+sleep 1
+echo "set_ftrace_filter enabled"
+
+echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable
+echo 1 > /sys/kernel/debug/tracing/events/sched/sched_wakeup/enable
+sleep 1
+
+echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_entry/enable
+echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_exit/enable
+echo "event enabled"
+
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+echo "tracing_on"
